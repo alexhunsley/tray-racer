f16ac48e02e86dae9a866c22ffd44680822461e8
Alex Hunsley <alex.hunsley@gmail.com>
2020-01-06 19:30:19 +0000

    Add gxui to show image in a x-platform window as opposed to Mac OS preview

--- github.com/alexhunsley/tray-racer/main.go
+++ github.com/alexhunsley/tray-racer/main.go
@@ -2,13 +2,12 @@ package main
 
 import (
 	"fmt"
+	"github.com/google/gxui"
+	"github.com/google/gxui/drivers/gl"
+	"github.com/google/gxui/themes/dark"
 	"image"
 	"image/color"
 	"image/draw"
-	"image/png"
-	"log"
-	"os"
-	"os/exec"
 )
 
 var (
@@ -20,35 +19,37 @@ var (
 func main() {
 	fmt.Println("Hello, Plain")
 
-	createImage()
+	gl.StartDriver(appMain)
 }
 
-func createImage() {
-	m := image.NewRGBA(image.Rect(0, 0, 640, 480)) //*NRGBA (image.Image interface)
-
-	// fill m in blue
+func appMain(driver gxui.Driver) {
+	width, height := 640, 480
+	m := image.NewRGBA(image.Rect(0, 0, width, height))
+	blue := color.RGBA{0, 0, 255, 255}
 	draw.Draw(m, m.Bounds(), &image.Uniform{blue}, image.ZP, draw.Src)
 
-	// draw a line
-	for i := m.Bounds().Min.X; i < m.Bounds().Max.X; i++ {
-		m.Set(i, m.Bounds().Max.Y/2, white) // to change a single pixel
-	}
-
-	w, _ := os.Create("testImage.png")
-	defer w.Close()
-	png.Encode(w, m) //Encode writes the Image m to w in PNG format.
-
-	Show(w.Name())
+	// The themes create the content. Currently only a dark theme is offered for GUI elements.
+	theme := dark.CreateTheme(driver)
+	img := theme.CreateImage()
+	window := theme.CreateWindow(width, height, "Image viewer")
+	texture := driver.CreateTexture(m, 1.0)
+	img.SetTexture(texture)
+	window.AddChild(img)
+	window.OnClose(driver.Terminate)
 }
 
-// show a specified file by Preview.app for OS X(darwin)
-func Show(name string) {
-	command := "open"
-	arg1 := "-a"
-	arg2 := "/Applications/Preview.app"
-	cmd := exec.Command(command, arg1, arg2, name)
-	err := cmd.Run()
-	if err != nil {
-		log.Fatal(err)
-	}
-}
+//func createImage() {
+//	m := image.NewRGBA(image.Rect(0, 0, 640, 480)) //*NRGBA (image.Image interface)
+//
+//	// fill m in blue
+//	draw.Draw(m, m.Bounds(), &image.Uniform{blue}, image.ZP, draw.Src)
+//
+//	// draw a line
+//	for i := m.Bounds().Min.X; i < m.Bounds().Max.X; i++ {
+//		m.Set(i, m.Bounds().Max.Y/2, white) // to change a single pixel
+//	}
+//
+//	//w, _ := os.Create("testImage.png")
+//	//defer w.Close()
+//	//png.Encode(w, m) //Encode writes the Image m to w in PNG format.
+//}
