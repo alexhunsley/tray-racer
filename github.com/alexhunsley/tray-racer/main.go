a2d827b5af3f2c6d135a8b8c22b1eae119f75b73
Hunsley.IO Ltd <alex.hunsley@gmail.com>
2020-01-09 18:21:00 +0000

    Mac OS: fix path to preview.app so we find it in either place

--- github.com/alexhunsley/tray-racer/main.go
+++ github.com/alexhunsley/tray-racer/main.go
@@ -43,10 +43,15 @@ func createImage() {
 
 // show a specified file by Preview.app for OS X(darwin)
 func Show(name string) {
+	previewCommandPath := "/System/Applications/Preview.app/Contents/MacOS/Preview"
+
+	if _, err := os.Stat(previewCommandPath); os.IsNotExist(err) {
+		previewCommandPath = "/Applications/Preview.app/Contents/MacOS/Preview"
+	}
 	command := "open"
 	arg1 := "-a"
-	arg2 := "/System/Applications/Preview.app/Contents/MacOS/Preview"
-	cmd := exec.Command(command, arg1, arg2, name)
+	//arg2 :=
+	cmd := exec.Command(command, arg1, previewCommandPath, name)
 	err := cmd.Run()
 	if err != nil {
 		log.Fatal(err)
